686fd38018185a7eca4100d31faec002dd5fdc9b
Jeremy Luchak <jeremy@nevada.unr.edu>
2019-01-31 18:51:59 -0800

    v2

--- src/main/java/com/gmail/ak1cec0ld/plugins/testplugin/TestPlugin.java
+++ src/main/java/com/gmail/ak1cec0ld/plugins/testplugin/TestPlugin.java
@@ -12,12 +12,25 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-
-import static org.bukkit.Bukkit.createBlockData;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class TestPlugin extends JavaPlugin implements CommandExecutor {
     private HashMap<String, Integer> activeToggles = new HashMap<>();
+    private Set<Biome> categorizedBiomes = new HashSet<>();
+    private HashMap<Biome,BlockData> biomeAssociations = new HashMap<>();
+    private final List<Material> availableMaterials = new ArrayList<>(Arrays.asList(Material.BLACK_STAINED_GLASS,Material.BLUE_STAINED_GLASS,
+                                                                                    Material.BROWN_STAINED_GLASS,Material.CYAN_STAINED_GLASS,
+                                                                                    Material.GREEN_STAINED_GLASS,Material.GRAY_STAINED_GLASS,
+                                                                                    Material.LIGHT_BLUE_STAINED_GLASS,Material.LIGHT_GRAY_STAINED_GLASS,
+                                                                                    Material.LIME_STAINED_GLASS,Material.MAGENTA_STAINED_GLASS,
+                                                                                    Material.ORANGE_STAINED_GLASS,Material.PINK_STAINED_GLASS,
+                                                                                    Material.PURPLE_STAINED_GLASS,Material.RED_STAINED_GLASS,
+                                                                                    Material.WHITE_STAINED_GLASS,Material.YELLOW_STAINED_GLASS));
 
     public void onEnable(){
         Bukkit.getLogger().info("Enabling TestPlugin");
@@ -52,28 +65,35 @@ public class TestPlugin extends JavaPlugin implements CommandExecutor {
             }
             activeToggles.clear();
         }
-        BlockData bd = createBlockData(Material.GLOWSTONE);
         int currentX = player.getLocation().getBlockX();
         int currentZ = player.getLocation().getBlockZ();
-        int radius = 40;
+        int radius = 50;
         for(int row = currentX-radius; row < currentX+radius; row++){
             for(int col = currentZ-radius; col < currentZ+radius; col++){
                 Block checking = player.getWorld().getHighestBlockAt(row,col).getRelative(BlockFace.UP);
+                Block checkNorth = checking.getRelative(BlockFace.NORTH);
+                Block checkEast = checking.getRelative(BlockFace.EAST);
+                
                 Biome b = checking.getBiome();
-                //Bukkit.getLogger().info(row + ", " + col + ":  " + b.toString());
-                if(!b.equals(checking.getRelative(BlockFace.NORTH).getBiome()) ||
-                        !b.equals(checking.getRelative(BlockFace.EAST).getBiome())){
-                    player.sendBlockChange(checking.getLocation(), bd);
+                Biome bn = checkNorth.getBiome();
+                Biome be = checkEast.getBiome();
+                categorize(b);
+                if(!b.equals(bn)){
+                    categorize(bn);
+                    player.sendBlockChange(checking.getLocation(), biomeAssociations.get(b));
+                    player.sendBlockChange(checkNorth.getLocation(), biomeAssociations.get(bn));
+                } 
+                if(!b.equals(be)){
+                    categorize(be);
+                    player.sendBlockChange(checking.getLocation(), biomeAssociations.get(b));
+                    player.sendBlockChange(checkEast.getLocation(), biomeAssociations.get(be));
                 }
             }
         }
     }
-/*
-get all biomes in radius
-assign them a block in existing set
-use that MAP to create borders
-
-
-
- */
+    private void categorize(Biome b){
+        if(this.categorizedBiomes.contains(b))return;
+        categorizedBiomes.add(b);
+        biomeAssociations.put(b, availableMaterials.get((categorizedBiomes.size()-1) % availableMaterials.size()).createBlockData());
+    }
 }
